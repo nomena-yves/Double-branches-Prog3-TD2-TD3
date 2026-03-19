3374a54d4d9e0057e387925234d50f6c7c14b7b6
nomena <hei.nomenjanahary@gmail.com>
2026-03-19 15:58:20 +0300

    formation du post et finition de get

--- src/main/java/hei/group/td2td3/Controllers/NameController.java
+++ src/main/java/hei/group/td2td3/Controllers/NameController.java
@@ -1,14 +1,32 @@
 package hei.group.td2td3.Controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import hei.group.td2td3.Entity.StudentsEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class NameController {
    @GetMapping("/welcome")
     public String welcomeName(@RequestParam String name) {
+       if(name==null){
+           return "Welcome ";
+       }
         return "welcome "+name;
+    }
+
+    List<StudentsEntity> listGeneral=new ArrayList<>();
+    @PostMapping("/students")
+    public List<String> addStudent(@RequestBody List<StudentsEntity> students) {
+        List<String> list=new ArrayList<>();
+        for(StudentsEntity student:students){
+            listGeneral.add(student);
+        }
+
+        for(StudentsEntity student:listGeneral){
+            list.add(student.getFirstName()+" "+student.getLastName());
+        }
+        return list;
     }
 }
