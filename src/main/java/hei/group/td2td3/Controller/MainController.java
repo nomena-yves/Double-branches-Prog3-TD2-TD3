88274e130e6130e17c02443f5e11fb9e74dbe54b
nomena <hei.nomenjanahary@gmail.com>
2026-03-20 09:18:03 +0300

    debut du TD3

--- src/main/java/hei/group/td2td3/Controller/MainController.java
+++ src/main/java/hei/group/td2td3/Controller/MainController.java
@@ -1,8 +1,18 @@
 package hei.group.td2td3.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-
+@GetMapping("/welcome")
+    public ResponseEntity <String> welcome(@RequestParam String name) {
+    if (name == null) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+    return ResponseEntity.ok("Welcome " + name);
+}
 }
